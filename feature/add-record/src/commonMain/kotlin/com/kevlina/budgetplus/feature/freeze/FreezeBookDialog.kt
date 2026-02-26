6541a9364e43677a1674ab0c00fbb5dc4e047fe8
Kevin Chiu <kevin.chiu@bandlab.com>
2026-02-26 19:35:37 +0800

    Update copies and add an image

--- feature/add-record/src/commonMain/kotlin/com/kevlina/budgetplus/feature/freeze/FreezeBookDialog.kt
+++ feature/add-record/src/commonMain/kotlin/com/kevlina/budgetplus/feature/freeze/FreezeBookDialog.kt
@@ -1,9 +1,12 @@
 package com.kevlina.budgetplus.feature.freeze
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -16,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
@@ -24,7 +28,10 @@ import budgetplus.core.common.generated.resources.Res
 import budgetplus.core.common.generated.resources.book_selection
 import budgetplus.core.common.generated.resources.cta_confirm
 import budgetplus.core.common.generated.resources.ic_arrow_drop_down
+import budgetplus.core.common.generated.resources.img_freeze_books
 import budgetplus.core.common.generated.resources.premium_unlock
+import budgetplus.core.common.generated.resources.record_freeze_book_description
+import budgetplus.core.common.generated.resources.record_freeze_book_title
 import com.kevlina.budgetplus.core.data.remote.Book
 import com.kevlina.budgetplus.core.theme.LocalAppColors
 import com.kevlina.budgetplus.core.ui.AppDialog
@@ -36,6 +43,7 @@ import com.kevlina.budgetplus.core.ui.FontSize
 import com.kevlina.budgetplus.core.ui.Icon
 import com.kevlina.budgetplus.core.ui.Text
 import com.kevlina.budgetplus.core.ui.rippleClick
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 
@@ -52,35 +60,38 @@ internal fun FreezeBookDialog(
         onDismissRequest = {},
     ) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .width(280.dp)
                 .wrapContentHeight()
                 .padding(all = 8.dp)
         ) {
+            Image(
+                painter = painterResource(Res.drawable.img_freeze_books),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Text(
-                //TODO: Update text
-                text = "重新購買？",
+                text = stringResource(Res.string.record_freeze_book_title),
                 fontSize = FontSize.SemiLarge,
                 fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center,
                 lineHeight = 24.sp
             )
 
-            Button(onClick = unlockPremium) {
-                Text(
-                    text = stringResource(Res.string.premium_unlock),
-                    color = LocalAppColors.current.light,
-                )
-            }
+            Spacer(modifier = Modifier.height(8.dp))
 
             Text(
-                //TODO: Update text
-                text = "您的帳本資料不會遺失，但您只能選擇一本帳本繼續記帳",
-                fontSize = FontSize.SemiLarge,
-                fontWeight = FontWeight.Medium,
+                text = stringResource(Res.string.record_freeze_book_description),
+                fontSize = FontSize.Normal,
+                textAlign = TextAlign.Center,
                 lineHeight = 24.sp
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
 
             var isSelectorShown by remember { mutableStateOf(false) }
             var selectedBook by remember { mutableStateOf(books.first()) }
@@ -130,11 +141,24 @@ internal fun FreezeBookDialog(
                 }
             }
 
-            Button(onClick = { activateBook(selectedBook.id) }) {
-                Text(
-                    text = stringResource(Res.string.cta_confirm),
-                    color = LocalAppColors.current.light,
-                )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                Button(onClick = unlockPremium) {
+                    Text(
+                        text = stringResource(Res.string.premium_unlock),
+                        color = LocalAppColors.current.light,
+                    )
+                }
+
+                Button(onClick = { activateBook(selectedBook.id) }) {
+                    Text(
+                        text = stringResource(Res.string.cta_confirm),
+                        color = LocalAppColors.current.light,
+                    )
+                }
             }
         }
     }
