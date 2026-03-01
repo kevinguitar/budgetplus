d5e560ceed21f4eb0cd8960fe2a023298e230737
Kevin Chiu <kevin.chiu@bandlab.com>
2026-03-01 10:20:46 +0800

    Clean up old paywall

--- core/billing/src/commonMain/kotlin/com/kevlina/budgetplus/core/billing/BillingController.kt
+++ core/billing/src/commonMain/kotlin/com/kevlina/budgetplus/core/billing/BillingController.kt
@@ -2,17 +2,11 @@ package com.kevlina.budgetplus.core.billing
 
 import com.revenuecat.purchases.kmp.models.CustomerInfo
 import com.revenuecat.purchases.kmp.ui.revenuecatui.PaywallListener
-import kotlinx.coroutines.flow.StateFlow
 
 interface BillingController {
-
-    val pricingMap: StateFlow<Map<PremiumPlan, Pricing?>>
 
     val paywallListener: PaywallListener
 
     fun onNewCustomerInfo(customerInfo: CustomerInfo)
 
-    fun fetchPrices()
-    fun purchase(plan: PremiumPlan)
-    fun restorePurchases()
 }
--- feature/unlock-premium/src/commonMain/kotlin/com/kevlina/budgetplus/feature/unlock/premium/PremiumScreen.kt
+++ feature/unlock-premium/src/commonMain/kotlin/com/kevlina/budgetplus/feature/unlock/premium/PremiumScreen.kt
@@ -1,14 +1,12 @@
 package com.kevlina.budgetplus.feature.unlock.premium
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import budgetplus.core.common.generated.resources.Res
@@ -16,7 +14,6 @@ import budgetplus.core.common.generated.resources.premium_unlock
 import com.kevlina.budgetplus.core.common.nav.BookDest
 import com.kevlina.budgetplus.core.common.nav.NavController
 import com.kevlina.budgetplus.core.theme.LocalAppColors
-import com.kevlina.budgetplus.core.ui.InfiniteCircularProgress
 import com.kevlina.budgetplus.core.ui.TopBar
 import com.kevlina.budgetplus.core.utils.metroViewModel
 import com.revenuecat.purchases.kmp.ui.revenuecatui.Paywall
@@ -27,7 +24,6 @@ import org.jetbrains.compose.resources.stringResource
 fun PremiumScreen(navController: NavController<BookDest>) {
 
     val vm = metroViewModel<PremiumViewModel>()
-//    val pricingMap by vm.pricingMap.collectAsStateWithLifecycle()
     val isPremium by vm.isPremium.collectAsStateWithLifecycle()
 
     LaunchedEffect(key1 = isPremium) {
@@ -54,26 +50,6 @@ fun PremiumScreen(navController: NavController<BookDest>) {
             navigateUp = navController::navigateUp
         )
 
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            InfiniteCircularProgress()
-
-            Paywall(options)
-        }
-
-//        Box(
-//            contentAlignment = Alignment.Center,
-//            modifier = Modifier
-//                .weight(1F)
-//                .fillMaxWidth()
-//        ) {
-//            PremiumContent(
-//                pricingMap = pricingMap,
-//                purchase = vm::purchase,
-//                restorePurchases = vm::restorePurchases
-//            )
-//        }
+        Paywall(options)
     }
 }
