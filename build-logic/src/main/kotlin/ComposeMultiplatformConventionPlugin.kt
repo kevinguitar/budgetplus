31326508ed5a8ab8ccb544332d6fc772e6547cba
Kevin Chiu <kevin.chiu@bandlab.com>
2026-03-21 21:25:04 +0800

    final fixes!

--- build-logic/src/main/kotlin/ComposeMultiplatformConventionPlugin.kt
+++ build-logic/src/main/kotlin/ComposeMultiplatformConventionPlugin.kt
@@ -34,11 +34,6 @@ class ComposeMultiplatformConventionPlugin : Plugin<Project> {
             }
         }
 
-        // See https://youtrack.jetbrains.com/issue/CMP-4885
-        project.tasks
-            .matching { it.name == "syncComposeResourcesForIos" }
-            .configureEach { enabled = false }
-
         project.dependencies {
             debugImplementation(project.libs.compose.android.uiTooling)
         }
