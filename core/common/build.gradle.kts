ff981c02050119e58336dfbd42a1c84b71e5d892
Kevin Chiu <kevin.chiu@bandlab.com>
2026-04-03 19:41:56 +0800

    Add a gradle task to copy traditional chinese strings

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -21,3 +21,28 @@ kotlin {
 compose.resources {
     publicResClass = true
 }
+
+// Create a task to copy the TW resources to HK and MO
+val copyTraditionalChinese = tasks.register<Copy>("copyTraditionalChinese") {
+    val baseResDir = "src/commonMain/composeResources"
+
+    into(baseResDir)
+
+    // Use TW as the single source of truth
+    from("$baseResDir/values-zh-rTW") {
+        into("values-zh-rHK")
+    }
+
+    from("$baseResDir/values-zh-rTW") {
+        into("values-zh-rMO")
+    }
+}
+
+tasks
+    .matching {
+        it.name.startsWith("convertXmlValueResources") ||
+            it.name.startsWith("copyNonXmlValueResources")
+    }
+    .configureEach {
+        dependsOn(copyTraditionalChinese)
+    }
