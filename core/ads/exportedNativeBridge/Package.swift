fb1935d84166f0d1282326744ac20381c3197f11
Kevin Chiu <kevin.chiu@bandlab.com>
2026-05-01 10:53:28 +0800

    1.9.1

--- core/ads/exportedNativeBridge/Package.swift
+++ core/ads/exportedNativeBridge/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["exportedNativeBridge"])
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.1.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0")
     ],
     targets: [
         .target(
