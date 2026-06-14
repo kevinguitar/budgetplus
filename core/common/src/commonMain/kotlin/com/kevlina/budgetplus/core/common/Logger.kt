acba4c14a6169d2d831cea7f9ccc421f88cfb1be
Kevin Chiu <kevin.chiu@bandlab.com>
2026-06-14 14:36:34 +0800

    Add internal Logger object

---
+++ core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/Logger.kt
@@ -0,0 +1,17 @@
+package com.kevlina.budgetplus.core.common
+
+import co.touchlab.kermit.Logger as KermitLogger
+
+object Logger {
+    fun d(message: String) = KermitLogger.d(message)
+
+    fun i(message: String) = KermitLogger.i(message)
+
+    fun w(message: String) = KermitLogger.w(message)
+
+    fun w(t: Throwable, message: String) = KermitLogger.w(t) { message }
+
+    fun e(message: String) = KermitLogger.w { message }
+
+    fun e(t: Throwable, message: String) = KermitLogger.e(t) { message }
+}
